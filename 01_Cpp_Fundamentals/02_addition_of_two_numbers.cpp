b92635b2ab9f2d9ac72d6e1aafc3188eca682fe7
Brahmanand-Bagewadi <bagewadibrahmanand@gmail.com>
2026-09-18 12:25:47 +0530

    Create 02_addition_of_two_numbers.cpp

---
+++ 01_Cpp_Fundamentals/02_addition_of_two_numbers.cpp
@@ -0,0 +1,41 @@
+/*
+============================================================
+Program Title : Addition of Two Numbers
+============================================================
+
+Student Name  : Brahmanand Nagappa Bagewadi
+Roll Number   : 353
+Division      : C
+SRN           : 01FE23BEC147
+Semester      : 7
+
+Concept Used:
+- Variables
+- User input using cin
+- Output using cout
+- Arithmetic operations
+
+Description:
+This program accepts two numbers from the user and
+calculates their sum using the addition operator.
+============================================================
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, sum;
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+  
+    sum = a + b;
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
