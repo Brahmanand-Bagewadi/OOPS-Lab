8590f5afb6848b9dd1ee0bb0cadc072242830ed4
Brahmanand-Bagewadi <bagewadibrahmanand@gmail.com>
2026-09-18 12:15:00 +0530

    Create 01_basic_output.cpp

---
+++ 01_Cpp_Fundamentals/01_basic_output.cpp
@@ -0,0 +1,32 @@
+/*
+============================================================
+Program Title : Basic Output Using cout
+============================================================
+
+Student Name  : Brahmanand Nagappa Bagewadi
+Roll Number   : 353
+Division      : C
+SRN           : 01FE23BEC147
+Semester      : 7
+
+Concept Used:
+- Basic C++ program structure
+- Input/Output stream
+- cout statement
+- main() function
+
+Description:
+This program demonstrates the basic structure of a C++
+program and displays a message on the console using cout.
+============================================================
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to Object Oriented Programming using C++!" << endl;
+
+    return 0;
+}
